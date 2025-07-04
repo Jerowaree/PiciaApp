89aadd839248c325385383be46eec15a90fe67cc
David <dqlizano18@gmail.com>
2025-07-03 20:47:29 -0500

    Se limpian los archivos necesarios, y se genera el codigo del home jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,1 +1,12 @@
 import React from 'react';
+import './Home.css';
+function Home() {
+    return (
+        <div>
+            <h1 className='main-title'>Los mejores postres de chocolate</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, debitis</p>
+        </div>
+    );
+}
+
+export default Home;
